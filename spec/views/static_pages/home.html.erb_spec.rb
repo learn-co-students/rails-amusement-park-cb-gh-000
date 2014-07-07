451fc58d5af8373e0abf7ce4d1191351a7dab5c3
Katie Hoffman <ktahoffman@gmail.com>
2014-07-07 17:37:42 -0400

    deletes other tests aside from features and models

--- spec/views/static_pages/home.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "static_pages/home", :type => :view do
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Welcome/)
-  end
-end
