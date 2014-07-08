4b1a661ede01e1bdefe81827e520c4a82c657262
Katie Hoffman <ktahoffman@gmail.com>
2014-07-08 11:47:01 -0400

    ends parenthesis

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -20,55 +20,55 @@ describe 'user signup', :type => :feature do
   end
 end
 
-# describe 'go on ride', :type => :feature do
+describe 'go on ride', :type => :feature do
 
-#   before :each do 
-#     @rollercoaster = Attraction.create(
-#       :name => "Roller Coaster",
-#       :tickets => 5,
-#       :nausea_rating => 2,
-#       :happiness_rating => 4,
-#       :min_height => 32
-#     )
-#     @ferriswheel = Attraction.create(
-#       :name => "Ferris Wheel",
-#       :tickets => 2,
-#       :nausea_rating => 2,
-#       :happiness_rating => 1,
-#       :min_height => 28
-#     )
-#   end
+  before :each do 
+    @rollercoaster = Attraction.create(
+      :name => "Roller Coaster",
+      :tickets => 5,
+      :nausea_rating => 2,
+      :happiness_rating => 4,
+      :min_height => 32
+    )
+    @ferriswheel = Attraction.create(
+      :name => "Ferris Wheel",
+      :tickets => 2,
+      :nausea_rating => 2,
+      :happiness_rating => 1,
+      :min_height => 28
+    )
+  end
 
-#   it 'has a link from the user show page to the attractions index page' do
-#     user_signup
-#     expect(page).to have_content("See rides")
-#     click_link('See rides')
-#   end
+  it 'has a link from the user show page to the attractions index page' do
+    user_signup
+    expect(page).to have_content("See rides")
+    click_link('See rides')
+  end
 
-#   it 'links from the user show page to the attractions index page' do
-#     user_signup
-#     click_link('See rides')
-#     expect(current_path).to eq('/attractions')
-#   end
+  it 'links from the user show page to the attractions index page' do
+    user_signup
+    click_link('See rides')
+    expect(current_path).to eq('/attractions')
+  end
 
-#   it 'has titles of the rides on the attractions index page' do
-#     user_signup
-#     click_link('See rides')
-#     expect(page).to have_content("#{@ferriswheel.name}")
-#     expect(page).to have_content("#{@rollercoaster.name}")
-#   end
+  it 'has titles of the rides on the attractions index page' do
+    user_signup
+    click_link('See rides')
+    expect(page).to have_content("#{@ferriswheel.name}")
+    expect(page).to have_content("#{@rollercoaster.name}")
+  end
 
-#   it "has links on the attractions index page to the attractions' show pages" do
-#     user_signup
-#     click_link('See rides')
-#     expect(page).to have_content("Go on #{@ferriswheel.name}")
-#     expect(page).to have_content("Go on #{@rollercoaster.name}")
-#   end
+  it "has links on the attractions index page to the attractions' show pages" do
+    user_signup
+    click_link('See rides')
+    expect(page).to have_content("Go on #{@ferriswheel.name}")
+    expect(page).to have_content("Go on #{@rollercoaster.name}")
+  end
 
-#   it "links from the attractions index page to the attractions' show pages" do
-#     user_signup
-#     click_link('See rides')
-#     click_link("Go on #{@ferriswheel.name}")
-#     expect(current_path).to eq("/attractions/#{@ferriswheel.id}"
-#   end
-# end
+  it "links from the attractions index page to the attractions' show pages" do
+    user_signup
+    click_link('See rides')
+    click_link("Go on #{@ferriswheel.name}")
+    expect(current_path).to eq("/attractions/2")
+  end
+end
