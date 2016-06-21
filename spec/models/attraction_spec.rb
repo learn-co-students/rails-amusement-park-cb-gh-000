84188589f713ef1870342144df24b860d5713c3a
Joshua Azemoh <azemoh@users.noreply.github.com>
2016-06-21 19:15:05 +0100

    Create test users with password

--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe Attraction, :type => :model do
   end
 
   it "has many users through rides" do
-    max = User.create(name: "Max Charles")
-    skai = User.create(name: "Skai Jackson")
+    max = User.create(name: "Max Charles", password: "password")
+    skai = User.create(name: "Skai Jackson", password: "password")
     @attraction.users << [max, skai]
     expect(@attraction.users.first).to eq(max)
     expect(@attraction.users.last).to eq(skai)
