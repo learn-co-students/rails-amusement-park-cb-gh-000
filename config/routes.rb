3077a9293242f0aabc48c6bdd3710df7485c9676
Abdelrauof <34385173+abdelrauof97@users.noreply.github.com>
2018-03-29 01:40:21 +0200

    Done something

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'static#home'
+  resources :users
+  get '/signin', to: 'sessions#new'
+  resources :sessions, except: [:new]
 
 end
--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,12 @@
 class CreateRides < ActiveRecord::Migration
-  # Write your migrations here
+
+  def change
+    create_table :rides do |t|
+      t.integer :user_id
+      t.integer :attraction_id
+
+      t.timestamps null: false
+    end
+  end
+
 end
