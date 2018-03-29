50329fa7d5166c303e54f0af740f83fb2b9c5792
Abdelrauof Mohammed <abdelrauof97@gmail.com>
2018-03-29 03:00:01 +0000

    Done

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,8 +2,7 @@ class RidesController < ApplicationController
 
   def new
     @ride = Ride.create( user_id: params[:user_id], attraction_id: params[:attraction_id] )
-    message = @ride.take_ride
-    redirect_to user_path(@ride.user, alert: message)
+    redirect_to user_path(@ride.user), alert: @ride.take_ride
   end
 
 end
